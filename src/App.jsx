dc1dcf3b50cf41ac46a6e93808dd8bc9ddbed28f
Siddharth734 <siddharthbaghel777@gmail.com>
2025-12-02 00:48:54 +0530

    Most basic react todo-list, playing with errorboundary,useState

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React,{ useState } from 'react'
+import './App.css'
+import ErrorBoundary from './errorBoundary';
+
+function TODO({title, done}) {
+  const [doneState, setDone] = useState(done);
+  
+  return(
+    <div className={doneState?"todo done":"todo"} onClick={() => setDone(!doneState)}>
+      {title}
+    </div>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: "go to gym",
+      done: false
+    },
+    {
+      title: "get hold of react",
+      done: false
+    },
+    {
+      title: "sleep all day",
+      done: true
+    }
+  ]);
+
+  const [inputval, setInputval] = useState("");
+
+  function addTodo() {
+    if(inputval===""){
+      alert("Empty input field! Enter a valid task!");
+      return;
+    }
+
+    setTodos([...todos,{
+      title: inputval,
+      done: false
+    }])
+  }
+
+  return (
+    <>
+      <main>
+        <h1>TODOS</h1>
+        <div className='addtodo'>
+          <input type="text" placeholder="new todo" value={inputval} onChange={(e) => setInputval(e.target.value)}/>
+          <button onClick={addTodo}>Add todo</button>
+        </div>
+        <div className='todoscontainer'>
+          {todos.map((todo,i) => <TODO key={i} title={todo.title} done={todo.done}></TODO>)}
+        </div>
+        <ErrorBoundary>
+          <Card1/>
+        </ErrorBoundary>
+      </main>
+    </>
+  )
+}
+
+function Card1() {
+  const[color, setColor] = useState(`rgb(0,0,0)`)
+  // throw new Error("hello");
+  let c1,c2,c3;
+  function changer(){
+    c1 = Math.floor(Math.random()*255);
+    c2 = Math.floor(Math.random()*255);
+    c3 = Math.floor(Math.random()*255);
+    setColor(`rgb(${c1},${c2},${c3})`);
+  }
+
+  return (
+    <div className='card' onClick={changer} style={{backgroundColor: color}}></div>
+  )
+}
+
+export default App
---
+++ src/errorBoundary.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+//A fallback component so that the wrhole app doesn't crash
+
+class ErrorBoundary extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error){
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info){
+        console.error("Error caught:", error, info);
+    }
+
+    render(){
+        if(this.state.hasError){
+            return <div className="card error">Something went wrong.</div>
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary
