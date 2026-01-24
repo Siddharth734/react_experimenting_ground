1e3ca5299680f72025cc8351431cce0d08d7c719
Siddharth734 <siddharthbaghel777@gmail.com>
2026-01-25 02:37:09 +0530

    fixed issues with img alignment with cursor

--- src/components/DiffBox.jsx
+++ src/components/DiffBox.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function DiffBox() {
     const [x, setX] = useState(0); 
-    const [y, setY] = useState(0); 
+    const [y, setY] = useState(0);
+    const diffBoxRef = useRef(null);
+
+    function Setter(e){
+        // here getBoundingClientRect() gets pos of diffbox relative to viewport, as clientX & clientY give mouse position relative to viewport
+        const diff = diffBoxRef.current.getBoundingClientRect();
+        setX(e.clientX - diff.x);
+        setY(e.clientY - diff.y);
+    }
     
     return(
-        <div className='diffbox' onMouseMove={(e) => { setX(e.clientX); setY(e.clientY); }}>
-            <img id='cursor'
-            style={{ top:`${y}px`, left:`${x}px`}}
-            src="https://img.icons8.com/?size=96&id=16477&format=png" alt="" />
+        <div className='diffbox' ref={diffBoxRef} onMouseMove={Setter}>
+            {
+                
+                <img id='cursor' style={{ top:`${y}px`, left:`${x}px`}} src="https://img.icons8.com/?size=96&id=16477&format=png" alt="" />
+            }
         </div>
     );
 }
