2decacc6bd0b3c9d88fe28af4e55171f777650f3
Siddharth734 <siddharthbaghel777@gmail.com>
2026-02-14 03:39:29 +0530

    added interactive horror twist to the spotlight effect

--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -1,29 +1,37 @@
 import { useRef, useState } from 'react';
+import horrorVid from "../assets/horror_spotlight.mp4";
+import horrorAud from "../assets/horror_spotlight_audio.wav";
 
 // based on linear & radial gradient
 export default function Spotlight() {
     const [x, setX] = useState(300); 
     const [y, setY] = useState(300);
     const boxRef = useRef(null);
+    const audRef = useRef(null);
     
     function Setter(e) {
         const offset = boxRef.current.getBoundingClientRect();
         setX(e.clientX - offset.x);
         setY(e.clientY - offset.y);
     }
+
+    function audioPlayer() {
+        audRef.current.play();
+    }
+
+    function audioStopper() {
+        audRef.current.pause();
+        audRef.current.currentTime = 0;
+    }
     
     return(
         <>
-            <div className="obox">
+            <div className="obox" onMouseEnter={audioPlayer} onMouseLeave={audioStopper}>
                 <div className="box" onMouseMove={Setter} ref={boxRef}
                 style={{ "--x": `${x}px`, "--y": `${y}px`}}>
-                    {/* <div className="ibox">BOX</div>
-                    <div className="ibox">BOX</div>
-                    <div className="ibox">BOX</div>
-                    <div className="ibox">BOX</div>
-                    <div className="ibox">BOX</div>
-                    <div className="ibox">BOX</div> */}
                 </div>
+                <video src={horrorVid} autoPlay loop muted playsInline></video>
+                <audio ref={audRef} src={horrorAud}></audio>
             </div>
         </>
     );
