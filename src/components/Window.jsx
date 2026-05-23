4c9f577cd229c40184146f6ec6c8d54d51a3edf9
Siddharth734 <siddharthbaghel777@gmail.com>
2026-05-23 23:53:41 +0530

    feat: add dynamic box positioning and styling in Window component

--- src/components/Window.jsx
+++ src/components/Window.jsx
@@ -4,7 +4,10 @@ import jungleImg from "../assets/jungle.jpg"
 export default function Window() {
     const [winVisible,setWinVisible] = useState(false);
     const [boxVisible, setBoxVisible] = useState(false);
+    const [x,setX] = useState(0);
+    const [y,setY] = useState(0);
     const winRef = useRef(null);
+    const boxRef = useRef(null);
 
     useEffect(() => {
 
@@ -19,15 +22,29 @@ export default function Window() {
             winRef.current.style.scale = 0.5;
     
         }
+
+        if(boxVisible){
+            boxRef.current.style.height = "20rem";
+            boxRef.current.style.width = "10rem";
+            boxRef.current.style.scale = 1;
+        }
+        else{
+            boxRef.current.style.height = "0rem";
+            boxRef.current.style.width = "0rem";
+            boxRef.current.style.scale = 0.5;
+    
+        }
     }, [winVisible, boxVisible]); 
     //re-renders every time winVisible is updated: bcs bV is part of dependancy array
 
-    function boxDisplay() {
+    function winDisplay() {
         setWinVisible(!winVisible);
     }
 
-    function Setter() {
+    function Setter(e) {
         setBoxVisible(!boxVisible);
+        setX((e.clientX));
+        setY((e.clientY));
     }
     
     return(
@@ -39,12 +56,13 @@ export default function Window() {
             }}>
                 <img src={jungleImg} alt="" />
                 <div className="window1" ref={winRef}></div>
-                <div className="box"></div>
+                <div className="box" ref={boxRef} style={{left:`${x}px`, top:`${y}px`}}>
+                </div>
             </div>
             <div className="bottom">
-                <i className="ri-microsoft-fill" onClick={boxDisplay}></i>
+                <i className="ri-microsoft-fill" onClick={winDisplay}></i>
                 <i className="ri-spotify-fill"></i>
-                <i class="ri-discord-fill"></i>
+                <i className="ri-discord-fill"></i>
             </div>
         </div>
     );
